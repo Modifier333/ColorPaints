a661925ac6a59215bfac7de0f57835fc194fd923
root <sony.tsi1122@gmail.com>
2023-07-10 13:08:16 +0530

    version 1.0

---
+++ jss/contact_script.js
@@ -0,0 +1,18 @@
+
+
+function openWa(){
+    
+    open("https://wa.me/+919550927922");
+
+
+}
+function openIg(){
+    open("https://instagram.com/color__paints?igshid=NGExMmI2YTkyZg==");
+
+
+}
+
+function openFb(){
+    open("https://www.facebook.com/profile.php?id=100094121926054&mibextid=ZbWKwL");
+
+}
---
+++ jss/help_script.js
@@ -0,0 +1,29 @@
+function inWebsite(){
+    open("mailto:your1stopsolution@outlook.com?subject=facing an issue in the website&body=Hello!  (describe your issue)");
+
+}
+function inService(){
+    open("mailto:your1stopsolution@outlook.com?subject=facing an issue in your service&body=Hello! (describe your issue with our Service)");
+    
+}
+
+function inContact(){
+    open("mailto:your1stopsolution@outlook.com?subject=facing an issue while Contacting&body=Hello! (describe your issue while Contacting Us)");
+    
+}
+
+function openWa(){
+    open("https://wa.me/+919550927922");
+
+
+}
+function openIg(){
+    open("https://instagram.com/color__paints?igshid=NGExMmI2YTkyZg==");
+
+
+}
+
+function openFb(){
+    open("https://www.facebook.com/profile.php?id=100094121926054&mibextid=ZbWKwL");
+
+}
